0dc14e019a212395091633efe2487008f682a7b9
Anastasia <abalyeva.a@gmail.com>
2024-10-02 21:49:03 +0300

    Update and rename zadanie 2.py to main.py

--- zadanie 2.py
+++
@@ -1,9 +0,0 @@
-print("введите два числа для их сравнения")
-num1=input("Первое число:")
-num2=input("Второе число:")
-num = num1 if num1<num2 else num2 
-if num1==num2 :
-    print("Числа равны")
-else:
-    print("Наименьшее число:", num)
-    
